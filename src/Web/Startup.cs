e662ce6aae9c9ee0d673eeb1bbdd647a921d2f50
Karol Szczawinski <karol.szczawinski@akmf.pl>
2020-04-20 20:43:03 +0200

    swagger config

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Web
 {
@@ -26,6 +27,11 @@ namespace Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Port API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +42,14 @@ namespace Web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Port API V1");
+                c.RoutePrefix = "swagger/ui";
+            });
+
 
             app.UseRouting();
 
