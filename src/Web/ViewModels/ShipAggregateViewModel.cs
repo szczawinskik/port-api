b7f7aafd7c56ce46f1ebe3dc0dca0041d94a1608
Karol Szczawinski <karol.szczawinski@akmf.pl>
2020-04-25 20:56:56 +0200

    Ship name required

--- src/Web/ViewModels/ShipAggregateViewModel.cs
+++ src/Web/ViewModels/ShipAggregateViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace Web.ViewModels
     public class ShipAggregateViewModel
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string ShipOwnerName { get; set; }
         public ScheduleViewModel ClosestSchedule { get; set; }
