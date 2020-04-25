fdcb7891626d4abe8fbd2d270622d7fefeb40112
Karol Szczawinski <karol.szczawinski@akmf.pl>
2020-04-25 12:13:24 +0200

    ScheduleSerivce Add method

---
+++ src/Infrastructure/Services/ScheduleService.cs
@@ -0,0 +1,34 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Services
+{
+    public class ScheduleService : IService<Schedule>
+    {
+        private IBaseRepository<Schedule> repository;
+        private ILogger<ScheduleService> logger;
+
+        public ScheduleService(IBaseRepository<Schedule> repository, ILogger<ScheduleService> logger)
+        {
+            this.repository = repository;
+            this.logger = logger;
+        }
+        public bool Add(Schedule item)
+        {
+            try
+            {
+                repository.Add(item);
+                return true;
+            }
+            catch(Exception e)
+            {
+                logger.LogError(e);
+            }
+            return false;
+        }
+    }
+}
---
+++ test/Infastructure.Tests/Services/ScheduleServiceTests.cs
@@ -0,0 +1,51 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Infrastructure.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infastructure.Tests.Services
+{
+    [TestFixture]
+    public class ScheduleServiceTests
+    {
+        private ScheduleService service;
+        private Mock<IBaseRepository<Schedule>> repositoryMock;
+        private Mock<ILogger<ScheduleService>> loggerMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            repositoryMock = new Mock<IBaseRepository<Schedule>>();
+            loggerMock = new Mock<ILogger<ScheduleService>>();
+
+            service = new ScheduleService(repositoryMock.Object, loggerMock.Object);
+        }
+        [Test]
+        public void ShouldAddToRepositoryAndReturnTrue()
+        {
+            var entity = new Schedule();
+
+            var result = service.Add(entity);
+
+            Assert.IsTrue(result);
+            repositoryMock.Verify(x => x.Add(entity), Times.Once());
+        }
+
+        [Test]
+        public void ShouldReturnFalseAndLogErrorWhenRepositoryAddFails()
+        {
+            var entity = new Schedule();
+            var expectedException = new Exception();
+            repositoryMock.Setup(x => x.Add(entity)).Throws(expectedException);
+
+            var result = service.Add(entity);
+
+            Assert.IsFalse(result);
+            loggerMock.Verify(x => x.LogError(expectedException), Times.Once());
+        }
+    }
+}
