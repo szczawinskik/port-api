d5eaf15f4f67f806aab58081fc7d21dc64b9eae9
Karol Szczawinski <karol.szczawinski@akmf.pl>
2020-04-25 12:35:24 +0200

    ApplicationContext Configuration

---
+++ src/Web/Configuration/ApplicationContextConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Database.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Configuration
+{
+    public static class ApplicationContextConfiguration
+    {
+        public static IServiceCollection ConfigureAppServices(this IServiceCollection collecion)
+        {
+            collecion.AddDbContext<ApplicationContext>(opt =>
+            {
+                opt.UseInMemoryDatabase("test_db");
+            });
+
+            return collecion;
+        }
+    }
+}
---
+++ src/Web/Configuration/RepositoriesConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Database.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Configuration
+{
+    public static class RepositoriesConfiguration
+    {
+        public static IServiceCollection ConfigureAppServices(this IServiceCollection collecion)
+        {
+            collecion.AddScoped<IBaseRepository<Schedule>, ScheduleRepository>();
+
+            return collecion;
+        }
+    }
+}
