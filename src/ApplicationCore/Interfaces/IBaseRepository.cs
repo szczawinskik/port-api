cefa7b0aa46b66c1053bf436351ad5077f93cfd1
Karol Szczawinski <karol.szczawinski@akmf.pl>
2020-04-25 09:52:59 +0200

    repository base

---
+++ src/ApplicationCore/Interfaces/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IBaseRepository<T> where T : EntityBase
+    {
+        IQueryable<T> GetAll();
+        T GetById(int id);
+        T Add(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+    }
+}
