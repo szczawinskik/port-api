b12af22f0f81c39d1d7c67b46bce7c60d5286133
Karol Szczawinski <karol.szczawinski@akmf.pl>
2020-04-25 12:30:46 +0200

    ApplicationContext

---
+++ src/Database/Context/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Context
+{
+    public class ApplicationContext : DbContext, IApplicationContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options) { }
+        public DbSet<ShipOwner> ShipOwners { get; set; }
+        public DbSet<Ship> Ships { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+    }
+}
---
+++ src/Database/Context/IApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Context
+{
+    public interface IApplicationContext
+    {
+        DbSet<ShipOwner> ShipOwners { get; set; }
+        DbSet<Ship> Ships { get; set; }
+        DbSet<Schedule> Schedules { get; set; }
+        int SaveChanges();
+    }
+}
