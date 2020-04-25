af7e0a693c0fb67366ed21f2f266211b5d38d32b
Karol Szczawinski <karol.szczawinski@akmf.pl>
2020-04-25 11:08:45 +0200

    controllers logic base

---
+++ src/Web/Controllers/AppController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+
+    public abstract class AppController<T> : GetController<T>
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public abstract IActionResult Add(T entity);
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public abstract void Update(T entity);
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public abstract void Delete(T entity);
+    }
+}
---
+++ src/Web/Controllers/GetController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
+    public abstract class GetController<T> : ControllerBase
+    {
+        public abstract IEnumerable<T> GetAll();
+        public abstract T GetById(int id);
+    }
+}
