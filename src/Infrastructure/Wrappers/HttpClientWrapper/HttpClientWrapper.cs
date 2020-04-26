2e00d0010a95421ab78a38c68caea2c75f2edc99
Karol Szczawinski <karol.szczawinski@akmf.pl>
2020-04-26 16:55:36 +0200

    HttpClientWrapper

---
+++ src/Infrastructure/Wrappers/HttpClientWrapper/HttpClientWrapper.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationCore.Messages;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Infrastructure.Wrappers.HttpClientWrapper
+{
+    public class HttpClientWrapper: IHttpClientWrapper
+    {
+        HttpClient client = new HttpClient();
+        public async Task<HttpResponseMessage> PostMessage(string requestUri, MessageBase message)
+        {
+           return await client.PostAsync(requestUri, PrepareMessage(message));
+        }
+
+        private HttpContent PrepareMessage(MessageBase message)
+        {
+            return new StringContent(JsonSerializer.Serialize(message));
+        }
+    }
+}
---
+++ src/Infrastructure/Wrappers/HttpClientWrapper/IHttpClientWrapper.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.Messages;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Wrappers.HttpClientWrapper
+{
+    public interface IHttpClientWrapper
+    {
+        Task<HttpResponseMessage> PostMessage(string requestUri, MessageBase message);
+    }
+}
