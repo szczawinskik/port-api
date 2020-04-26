82d2f7e2177c3b98212388b32360a78a691d6706
Karol Szczawinski <karol.szczawinski@akmf.pl>
2020-04-26 07:47:26 +0200

    Repository split

--- src/ApplicationCore/Interfaces/IBaseRepository.cs
+++ src/ApplicationCore/Interfaces/IBaseRepository.cs
@@ -6,10 +6,9 @@ using System.Text;
 
 namespace ApplicationCore.Interfaces
 {
-    public interface IBaseRepository<T> where T : EntityBase
+    public interface IBaseRepository<T>: IFindRepository<T>  where T : EntityBase
     {
         IQueryable<T> GetAll();
-        T Find(int id);
         void Add(T entity, int parentId);
         void Update(T entity);
         void Delete(int id);
