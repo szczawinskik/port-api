140c9830fab2410d82ad332e9d98a020194e70d0
Karol Szczawinski <karol.szczawinski@akmf.pl>
2020-04-26 10:10:25 +0200

    ConfigurationController

---
+++ src/Web/Controllers/ConfigurationController.cs
@@ -0,0 +1,52 @@
+﻿using ApplicationCore.Entities;
+using Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Web.Validation.Interfaces;
+using Web.ViewModels;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class ConfigurationController : ControllerBase
+    {
+        private readonly IConfigurationService configurationService;
+        private readonly IValidator<RemoteServiceAddressViewModel> validator;
+
+        public ConfigurationController(IConfigurationService configurationService,
+            IValidator<RemoteServiceAddressViewModel> validator)
+        {
+            this.configurationService = configurationService;
+            this.validator = validator;
+        }
+        [HttpGet("[action]")]
+        public string RemoteServiceAddress()
+        {
+            return configurationService.GetConfigurationValue(ConfigurationType.RemoteServiceAddress);
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult RemoteServiceAddress(RemoteServiceAddressViewModel model)
+        {
+            if (validator.IsValid(model))
+            {
+                if (configurationService.SetConfigurationValue(ConfigurationType.RemoteServiceAddress, model.Value))
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            return BadRequest(validator.ErrorList);
+
+        }
+    }
+}
---
+++ test/Web.Tests/Controllers/ConfigurationControllerTests.cs
@@ -0,0 +1,89 @@
+﻿using Infrastructure.Interfaces;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Web.Controllers;
+using Web.Validation.Interfaces;
+using Web.ViewModels;
+using ApplicationCore.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Tests.Controllers
+{
+    [TestFixture]
+    public class ConfigurationControllerTests
+    {
+        private Mock<IConfigurationService> serviceMock;
+        private Mock<IValidator<RemoteServiceAddressViewModel>> validatorMock;
+        private ConfigurationController controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            serviceMock = new Mock<IConfigurationService>();
+            validatorMock = new Mock<IValidator<RemoteServiceAddressViewModel>>();
+
+            controller = new ConfigurationController(serviceMock.Object, validatorMock.Object);
+        }
+
+        [Test]
+        public void ShouldReturnRemoteServiceAddress()
+        {
+            var address = "address";
+            serviceMock.Setup(x => x.GetConfigurationValue(ConfigurationType.RemoteServiceAddress))
+                .Returns(address);
+
+            var result = controller.RemoteServiceAddress();
+
+            Assert.AreEqual(address, result);
+            serviceMock.Verify(x => x.GetConfigurationValue(ConfigurationType.RemoteServiceAddress),
+                Times.Once());
+        }
+
+        [Test]
+        public void ShouldReturnBadRequestAndValidationErrorsWhenModelIsNotValid()
+        {
+            var model = new RemoteServiceAddressViewModel();
+            validatorMock.Setup(x => x.IsValid(model)).Returns(false);
+            var errorsList = new List<string>();
+            validatorMock.SetupGet(x => x.ErrorList).Returns(errorsList);
+
+            var result = controller.RemoteServiceAddress(model);
+
+            Assert.IsInstanceOf<BadRequestObjectResult>(result);
+            var badReqest = result as BadRequestObjectResult;
+            Assert.AreEqual(errorsList, badReqest.Value);
+        }
+        [Test]
+        public void ShouldAddItemWhenModelIsValid()
+        {
+            var model = new RemoteServiceAddressViewModel {Value = "address" };
+            validatorMock.Setup(x => x.IsValid(model)).Returns(true);
+            serviceMock.Setup(x => x.SetConfigurationValue(ConfigurationType.RemoteServiceAddress, model.Value))
+                .Returns(true);
+
+            var result = controller.RemoteServiceAddress(model);
+
+            Assert.IsInstanceOf<OkResult>(result);
+            serviceMock.Verify(x => x.SetConfigurationValue(ConfigurationType.RemoteServiceAddress, model.Value),
+                Times.Once());
+        }
+
+        [Test]
+        public void ShouldReturnBadRequestWhenAddFails()
+        {
+            var model = new RemoteServiceAddressViewModel {Value = "address" };
+            validatorMock.Setup(x => x.IsValid(model)).Returns(true);
+            serviceMock.Setup(x => x.SetConfigurationValue(ConfigurationType.RemoteServiceAddress, model.Value))
+              .Returns(false);
+
+            var result = controller.RemoteServiceAddress(model);
+
+            Assert.IsInstanceOf<BadRequestResult>(result);
+            serviceMock.Verify(x => x.SetConfigurationValue(ConfigurationType.RemoteServiceAddress, model.Value),
+               Times.Once());
+        }
+    }
+}
