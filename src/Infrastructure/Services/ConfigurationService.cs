f0d868d806ec230d7421dc9abc1289fcf551122a
Karol Szczawinski <karol.szczawinski@akmf.pl>
2020-04-26 10:19:20 +0200

    ConfigurationService

---
+++ src/Infrastructure/Services/ConfigurationService.cs
@@ -0,0 +1,47 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Services
+{
+    public class ConfigurationService : IConfigurationService
+    {
+        private readonly IConfigurationRepository repository;
+        private readonly IApplicationLogger<ConfigurationService> logger;
+
+        public ConfigurationService(IConfigurationRepository repository, IApplicationLogger<ConfigurationService> logger)
+        {
+            this.repository = repository;
+            this.logger = logger;
+        }
+        public string GetConfigurationValue(ConfigurationType configurationType)
+        {
+            try
+            {
+                return repository.GetConfigurationValue(configurationType);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e);
+            }
+            return null;
+        }
+
+        public bool SetConfigurationValue(ConfigurationType configurationType, string value)
+        {
+            try
+            {
+                repository.SetConfigurationValue(configurationType, value);
+                return true;
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e);
+            }
+            return false;
+        }
+    }
+}
---
+++ test/Infastructure.Tests/Services/ConfigurationServiceTests.cs
@@ -0,0 +1,82 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Infrastructure.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infastructure.Tests.Services
+{
+    [TestFixture]
+    public class ConfigurationServiceTests
+    {
+        private Mock<IConfigurationRepository> repositoryMock;
+        private Mock<IApplicationLogger<ConfigurationService>> loggerMock;
+        private ConfigurationService service;
+
+        [SetUp]
+        public void Setup()
+        {
+            repositoryMock = new Mock<IConfigurationRepository>();
+            loggerMock = new Mock<IApplicationLogger<ConfigurationService>>();
+
+            service = new ConfigurationService(repositoryMock.Object, loggerMock.Object);
+        }
+
+        [Test]
+        public void ShoulReturnConfigurationValue()
+        {
+            var configurationType = ConfigurationType.RemoteServiceAddress;
+            var configValue = "aaa";
+            repositoryMock.Setup(x => x.GetConfigurationValue(configurationType))
+                .Returns(configValue);
+
+            var result = service.GetConfigurationValue(configurationType);
+
+            Assert.AreEqual(configValue, result);
+            repositoryMock.Verify(x => x.GetConfigurationValue(configurationType), Times.Once());
+        }
+
+        [Test]
+        public void ShoulReturnNullAndCatchExceptionWhenGetFails()
+        {
+            var configurationType = ConfigurationType.RemoteServiceAddress;
+            var expectedException = new Exception();
+            repositoryMock.Setup(x => x.GetConfigurationValue(configurationType))
+                .Throws(expectedException);
+
+            var result = service.GetConfigurationValue(configurationType);
+
+            Assert.IsNull(result);
+            loggerMock.Verify(x => x.LogError(expectedException), Times.Once());
+        }
+        [Test]
+        public void ShoulSetConfigurationValue()
+        {
+            var configurationType = ConfigurationType.RemoteServiceAddress;
+            var configValue = "aaa";
+
+            var result = service.SetConfigurationValue(configurationType, configValue);
+
+            Assert.IsTrue(result);
+            repositoryMock.Verify(x => x.SetConfigurationValue(configurationType, configValue), Times.Once());
+        }
+
+        [Test]
+        public void ShoulReturnFalseAndCatchExceptionWhenSetFails()
+        {
+            var configurationType = ConfigurationType.RemoteServiceAddress;
+            var configValue = "aaa";
+            var expectedException = new Exception();
+            repositoryMock.Setup(x => x.SetConfigurationValue(configurationType, configValue))
+                .Throws(expectedException);
+
+            var result = service.SetConfigurationValue(configurationType, configValue);
+
+            Assert.IsFalse(result);
+            loggerMock.Verify(x => x.LogError(expectedException), Times.Once());
+        }
+    }
+}
